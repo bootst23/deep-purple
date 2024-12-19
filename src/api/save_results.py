0775b673e1e7777caf0ee2643c3ce17bdb8b99c6
MaymnKhin <maymnkhin38@gmail.com>
2024-12-20 04:07:24 +0800

    CORS

--- src/api/save_results.py
+++ src/api/save_results.py
@@ -20,7 +20,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://deep-purple-site.onrender.com, http://localhost:5173"],
+    allow_origins=["https://deep-purple-site.onrender.com", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
